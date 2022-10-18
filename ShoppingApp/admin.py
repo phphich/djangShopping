40f461648cc22845698c602c146128760bd8917e
Phichayaphak <ph_phich@hotmail.com>
2022-10-18 22:54:14 +0700

    first commit

---
+++ ShoppingApp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from ShoppingApp.models import *
+# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(Employees)
+admin.site.register(Customers)
+admin.site.register(Orders)
+admin.site.register(OrderDetails)
+admin.site.register(Transfers)
+
+
+
